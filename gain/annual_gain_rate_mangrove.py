d8a5e14a5088d3d7f1f7eec12d7760a5f13dec65
JOY DEEP SAHA <88198830+sahajoydeep2002@users.noreply.github.com>
2021-09-11 22:42:46 +0530

    Changed entire model (main model and pre-processing) to compress using

---
+++ gain/annual_gain_rate_mangrove.py
@@ -0,0 +1,148 @@
+### Creates tiles of annual aboveground and belowground biomass gain rates for mangroves using IPCC Wetlands Supplement Table 4.4 rates.
+### Its inputs are the continent-ecozone tiles, mangrove biomass tiles (for locations of mangroves), and the IPCC
+### gain rate table.
+
+import datetime
+import numpy as np
+import os
+import rasterio
+import sys
+sys.path.append('../')
+import constants_and_names as cn
+import universal_util as uu
+
+# Necessary to suppress a pandas error later on. https://github.com/numpy/numpy/issues/12987
+np.set_printoptions(threshold=sys.maxsize)
+
+def annual_gain_rate(tile_id, sensit_type, output_pattern_list, gain_above_dict, gain_below_dict, stdev_dict):
+
+    uu.print_log("Processing:", tile_id)
+
+    # Start time
+    start = datetime.datetime.now()
+
+    # This is only needed for testing, when a list of tiles might include ones without mangroves.
+    # When the full model is being run, only tiles with mangroves are included.
+    mangrove_biomass_tile_list = uu.tile_list_s3(cn.mangrove_biomass_2000_dir)
+    if tile_id not in mangrove_biomass_tile_list:
+        uu.print_log("{} does not contain mangroves. Skipping tile.".format(tile_id))
+        return
+
+    # Name of the input files
+    mangrove_biomass = uu.sensit_tile_rename(sensit_type, tile_id, cn.pattern_mangrove_biomass_2000)
+    cont_eco = uu.sensit_tile_rename(sensit_type, tile_id, cn.pattern_cont_eco_processed)
+
+    # Names of the output aboveground and belowground mangrove gain rate tiles
+    AGB_gain_rate = '{0}_{1}.tif'.format(tile_id, output_pattern_list[0])
+    BGB_gain_rate = '{0}_{1}.tif'.format(tile_id, output_pattern_list[1])
+    AGB_gain_stdev = '{0}_{1}.tif'.format(tile_id, output_pattern_list[2])
+
+    uu.print_log("  Reading input files and creating aboveground and belowground biomass gain rates for {}".format(tile_id))
+
+    cont_eco_src = rasterio.open(cont_eco)
+    mangrove_AGB_src = rasterio.open(mangrove_biomass)
+
+    # Grabs metadata about the tif, like its location/projection/cellsize
+    kwargs = cont_eco_src.meta
+
+    # Grabs the windows of the tile (stripes) to iterate over the entire tif without running out of memory
+    windows = cont_eco_src.block_windows(1)
+
+    # Updates kwargs for the output dataset.
+    # Need to update data type to float 32 so that it can handle fractional gain rates
+    kwargs.update(
+        driver='GTiff',
+        count=1,
+        compress='DEFLATE',
+        nodata=0,
+        dtype='float32'
+    )
+
+    dst_above = rasterio.open(AGB_gain_rate, 'w', **kwargs)
+    # Adds metadata tags to the output raster
+    uu.add_rasterio_tags(dst_above, sensit_type)
+    dst_above.update_tags(
+        units='megagrams aboveground biomass (AGB or dry matter)/ha/yr')
+    dst_above.update_tags(
+        source='IPCC Guidelines, 2013 Coastal Wetlands Supplement, Table 4.4')
+    dst_above.update_tags(
+        extent='Simard et al. 2018, based on Giri et al. 2011 (Global Ecol. Biogeogr.) mangrove extent')
+
+    dst_below = rasterio.open(BGB_gain_rate, 'w', **kwargs)
+    # Adds metadata tags to the output raster
+    uu.add_rasterio_tags(dst_below, sensit_type)
+    dst_below.update_tags(
+        units='megagrams belowground biomass (BGB or dry matter)/ha/yr')
+    dst_below.update_tags(
+        source='IPCC Guidelines, 2013 Coastal Wetlands Supplement, Table 4.4')
+    dst_below.update_tags(
+        extent='Simard et al. 2018, based on Giri et al. 2011 (Global Ecol. Biogeogr.) mangrove extent')
+
+    dst_stdev_above = rasterio.open(AGB_gain_stdev, 'w', **kwargs)
+    # Adds metadata tags to the output raster
+    uu.add_rasterio_tags(dst_stdev_above, sensit_type)
+    dst_stdev_above.update_tags(
+        units='standard deviation, in terms of megagrams aboveground biomass (AGB or dry matter)/ha/yr')
+    dst_stdev_above.update_tags(
+        source='IPCC Guidelines, 2013 Coastal Wetlands Supplement, Table 4.4')
+    dst_stdev_above.update_tags(
+        extent='Simard et al. 2018, based on Giri et al. 2011 (Global Ecol. Biogeogr.) mangrove extent')
+
+    # Iterates across the windows (1 pixel strips) of the input tile
+    for idx, window in windows:
+
+        # Creates windows for each input raster
+        cont_eco = cont_eco_src.read(1, window=window)
+        mangrove_AGB = mangrove_AGB_src.read(1, window=window)
+
+        # Converts the continent-ecozone array to float so that the values can be replaced with fractional gain rates.
+        # Creates two copies: one for aboveground gain and one for belowground gain.
+        # Creating only one copy of the cont_eco raster made it so that belowground gain rates weren't being
+        # written correctly for some reason.
+        cont_eco_above = cont_eco.astype('float32')
+        cont_eco_below = cont_eco.astype('float32')
+        cont_eco_stdev = cont_eco.astype('float32')
+
+        # Reclassifies mangrove biomass to 1 or 0 to make a mask of mangrove pixels.
+        # Ultimately, only these pixels (ones with mangrove biomass) will get values.
+        mangrove_AGB[mangrove_AGB > 0] = 1
+
+
+        # Applies the dictionary of continent-ecozone aboveground gain rates to the continent-ecozone array to
+        # get annual aboveground gain rates (metric tons aboveground biomass/yr) for each pixel
+        for key, value in gain_above_dict.items():
+            cont_eco_above[cont_eco_above == key] = value
+
+        # Masks out pixels without mangroves, leaving gain rates in only pixels with mangroves
+        dst_above_data = cont_eco_above * mangrove_AGB
+
+        # Writes the output window to the output
+        dst_above.write_band(1, dst_above_data, window=window)
+
+
+        # Same as above but for belowground gain rates
+        for key, value in gain_below_dict.items():
+            cont_eco_below[cont_eco_below == key] = value
+
+        dst_below_data = cont_eco_below * mangrove_AGB
+
+        dst_below.write_band(1, dst_below_data, window=window)
+
+
+        # Applies the dictionary of continent-ecozone aboveground gain rate standard deviations to the continent-ecozone array to
+        # get annual aboveground gain rate standard deviations (metric tons aboveground biomass/yr) for each pixel
+        for key, value in stdev_dict.items():
+            cont_eco_stdev[cont_eco_stdev == key] = value
+
+        # Masks out pixels without mangroves, leaving gain rates in only pixels with mangroves
+        dst_stdev = cont_eco_stdev * mangrove_AGB
+
+        # Writes the output window to the output
+        dst_stdev_above.write_band(1, dst_stdev, window=window)
+
+    # Prints information about the tile that was just processed
+    uu.end_of_fx_summary(start, tile_id, output_pattern_list[0])
+
+
+
+
---
+++ gain/mp_forest_age_category_IPCC.py
@@ -0,0 +1,174 @@
+'''
+This script creates tiles of forest age category across the entire model extent (all pixels) according to a decision tree.
+The age categories are: <= 20 year old secondary forest (1), >20 year old secondary forest (2), and primary forest (3).
+The decision tree is implemented as a series of numpy array statements rather than as nested if statements or gdal_calc operations.
+The output tiles have 3 possible values, each value representing an end of the decision tree.
+The forest age category tiles are inputs for assigning gain rates to
+non-mangrove, non-planted, non-European, non-US, older secondary and primary forests.pixels.
+The extent of this layer is greater than the extent of the rates which are based on this, though.
+This assigns forest age category to all pixels within the model but they are ultimately only used for
+non-mangrove, non-planted, non-European, non-US, older secondary and primary forest pixels.
+You can think of the output from this script as being the age category if IPCC Table 4.9 rates were to be applied there.
+'''
+
+
+import multiprocessing
+from functools import partial
+import pandas as pd
+import datetime
+import argparse
+from subprocess import Popen, PIPE, STDOUT, check_call
+import os
+import sys
+sys.path.append('../')
+import constants_and_names as cn
+import universal_util as uu
+sys.path.append(os.path.join(cn.docker_app,'gain'))
+import forest_age_category_IPCC
+
+def mp_forest_age_category_IPCC(sensit_type, tile_id_list, run_date = None, no_upload = None):
+
+    os.chdir(cn.docker_base_dir)
+
+    # If a full model run is specified, the correct set of tiles for the particular script is listed
+    if tile_id_list == 'all':
+        # List of tiles to run in the model
+        tile_id_list = uu.tile_list_s3(cn.model_extent_dir, sensit_type)
+
+    uu.print_log(tile_id_list)
+    uu.print_log("There are {} tiles to process".format(str(len(tile_id_list))) + "\n")
+
+
+    # Files to download for this script.
+    download_dict = {
+                     cn.model_extent_dir: [cn.pattern_model_extent],
+                     cn.gain_dir: [cn.pattern_gain],
+                     cn.ifl_primary_processed_dir: [cn.pattern_ifl_primary],
+                     cn.cont_eco_dir: [cn.pattern_cont_eco_processed]
+    }
+
+    # Adds the correct loss tile to the download dictionary depending on the model run
+    if sensit_type == 'legal_Amazon_loss':
+        download_dict[cn.Brazil_annual_loss_processed_dir] = [cn.pattern_Brazil_annual_loss_processed]
+    elif sensit_type == 'Mekong_loss':
+        download_dict[cn.Mekong_loss_processed_dir] = [cn.pattern_Mekong_loss_processed]
+    else:
+        download_dict[cn.loss_dir] = [cn.pattern_loss]
+
+    # Adds the correct biomass tile to the download dictionary depending on the model run
+    if sensit_type == 'biomass_swap':
+        download_dict[cn.JPL_processed_dir] = [cn.pattern_JPL_unmasked_processed]
+    else:
+        download_dict[cn.WHRC_biomass_2000_unmasked_dir] = [cn.pattern_WHRC_biomass_2000_unmasked]
+
+
+    # List of output directories and output file name patterns
+    output_dir_list = [cn.age_cat_IPCC_dir]
+    output_pattern_list = [cn.pattern_age_cat_IPCC]
+
+
+    # Downloads input files or entire directories, depending on how many tiles are in the tile_id_list
+    for key, values in download_dict.items():
+        dir = key
+        pattern = values[0]
+        uu.s3_flexible_download(dir, pattern, cn.docker_base_dir, sensit_type, tile_id_list)
+
+
+    # If the model run isn't the standard one, the output directory and file names are changed
+    if sensit_type != 'std':
+        uu.print_log("Changing output directory and file name pattern based on sensitivity analysis")
+        output_dir_list = uu.alter_dirs(sensit_type, output_dir_list)
+        output_pattern_list = uu.alter_patterns(sensit_type, output_pattern_list)
+
+    # A date can optionally be provided by the full model script or a run of this script.
+    # This replaces the date in constants_and_names.
+    # Only done if output upload is enabled.
+    if run_date is not None and no_upload is not None:
+        output_dir_list = uu.replace_output_dir_date(output_dir_list, run_date)
+
+
+    # Table with IPCC Table 4.9 default gain rates
+    cmd = ['aws', 's3', 'cp', os.path.join(cn.gain_spreadsheet_dir, cn.gain_spreadsheet), cn.docker_base_dir, '--no-sign-request']
+    uu.log_subprocess_output_full(cmd)
+
+
+    # Imports the table with the ecozone-continent codes and the carbon gain rates
+    gain_table = pd.read_excel("{}".format(cn.gain_spreadsheet),
+                               sheet_name = "natrl fores gain, for std model")
+
+    # Removes rows with duplicate codes (N. and S. America for the same ecozone)
+    gain_table_simplified = gain_table.drop_duplicates(subset='gainEcoCon', keep='first')
+
+    # Converts the continent-ecozone codes and young forest gain rates to a dictionary
+    gain_table_dict = pd.Series(gain_table_simplified.growth_secondary_less_20.values,index=gain_table_simplified.gainEcoCon).to_dict()
+
+    # Adds a dictionary entry for where the ecozone-continent code is 0 (not in a continent)
+    gain_table_dict[0] = 0
+
+
+    # Creates a single filename pattern to pass to the multiprocessor call
+    pattern = output_pattern_list[0]
+
+    # This configuration of the multiprocessing call is necessary for passing multiple arguments to the main function
+    # It is based on the example here: http://spencerimp.blogspot.com/2015/12/python-multiprocess-with-multiple.html
+    # With processes=30, peak usage was about 350 GB using WHRC AGB.
+    # processes=26 maxes out above 480 GB for biomass_swap, so better to use fewer than that.
+    if cn.count == 96:
+        if sensit_type == 'biomass_swap':
+            processes = 32 # 32 processors = 610 GB peak
+        else:
+            processes = 42 # 30 processors=460 GB peak; 36 = 550 GB peak; 40 = XXX GB peak
+    else:
+        processes = 2
+    uu.print_log('Natural forest age category max processors=', processes)
+    pool = multiprocessing.Pool(processes)
+    pool.map(partial(forest_age_category_IPCC.forest_age_category, gain_table_dict=gain_table_dict,
+                     pattern=pattern, sensit_type=sensit_type, no_upload=no_upload), tile_id_list)
+    pool.close()
+    pool.join()
+
+    # # For single processor use
+    # for tile_id in tile_id_list:
+    #
+    #     forest_age_category_IPCC.forest_age_category(tile_id, gain_table_dict, pattern, sensit_type, no_upload)
+
+
+    # If no_upload flag is not activated (by choice or by lack of AWS credentials), output is uploaded
+    if not no_upload:
+
+        uu.upload_final_set(output_dir_list[0], output_pattern_list[0])
+
+
+if __name__ == '__main__':
+
+    # The arguments for what kind of model run is being run (standard conditions or a sensitivity analysis) and
+    # the tiles to include
+    parser = argparse.ArgumentParser(
+        description='Create tiles of the forest age category (<20 years, >20 years secondary, primary)')
+    parser.add_argument('--model-type', '-t', required=True,
+                        help='{}'.format(cn.model_type_arg_help))
+    parser.add_argument('--tile_id_list', '-l', required=True,
+                        help='List of tile ids to use in the model. Should be of form 00N_110E or 00N_110E,00N_120E or all.')
+    parser.add_argument('--run-date', '-d', required=False,
+                        help='Date of run. Must be format YYYYMMDD.')
+    parser.add_argument('--no-upload', '-nu', action='store_true',
+                       help='Disables uploading of outputs to s3')
+    args = parser.parse_args()
+    sensit_type = args.model_type
+    tile_id_list = args.tile_id_list
+    run_date = args.run_date
+    no_upload = args.no_upload
+
+    # Disables upload to s3 if no AWS credentials are found in environment
+    if not uu.check_aws_creds():
+        no_upload = True
+
+    # Create the output log
+    uu.initiate_log(tile_id_list=tile_id_list, sensit_type=sensit_type, run_date=run_date, no_upload=no_upload)
+
+    # Checks whether the sensitivity analysis and tile_id_list arguments are valid
+    uu.check_sensit_type(sensit_type)
+    tile_id_list = uu.tile_id_list_check(tile_id_list)
+
+    mp_forest_age_category_IPCC(sensit_type=sensit_type, tile_id_list=tile_id_list, run_date=run_date, no_upload=no_upload)
+
